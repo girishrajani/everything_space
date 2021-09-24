10ea08b47cb45323225dbaa80beb36d18e46e85b
Girish Rajani <girish.r.rajani@gmail.com>
2021-09-24 22:22:36 +0530

    HomeScreen Model and Data Added

---
+++ lib/data/home_creen_data.dart
@@ -0,0 +1,8 @@
+import '../models/home_model.dart';
+
+final homeData = [
+  DrawerItem(title: 'News', image: ''),
+  DrawerItem(title: 'Internation Space Station', image: ''),
+  DrawerItem(title: 'Tiangong Space Station', image: ''),
+  DrawerItem(title: 'CosmoClub', image: ''),
+];
---
+++ lib/models/home_model.dart
@@ -0,0 +1,9 @@
+class DrawerItem {
+  final String title;
+  final String image;
+
+  const DrawerItem({
+    required this.title,
+    required this.image,
+  });
+}
